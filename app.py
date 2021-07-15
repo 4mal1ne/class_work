dab3d5d1fa6183529f490a08e72e22f28e17337b
4mal1ne <parieiko.vlad@gmail.com>
2021-07-15 20:52:34 +0300

    Comment 'app.start'

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 import sys
 
+from settings import LOGIN, PASSWORD
+
 
 def start():
-    print(f'Hello, {sys.argv[-1]}')
+    print(f'Hello, {sys.argv[-2]}')
+
+
+template = sys.argv
+
+
+def check_user():
+    if template[-2] == LOGIN:
+        if template[-1] == PASSWORD:
+            start()
